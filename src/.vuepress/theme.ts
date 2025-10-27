d111d609e5ed83a9083cf2a33b3ff2d04247ef4c
warm <290631660@qq.com>
2025-10-27 13:35:33 +0800

    [upgrade] warm-flow 升级1.8.3

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -56,7 +56,7 @@ export default hopeTheme(
             notice: [
                 {
                     path: "/master",
-                    title: "公告: 1.8.3-m4（公测版）",
+                    title: "公告: 1.8.3）",
                     content:
                         `
                          <table>
